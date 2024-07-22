046faeaa70b5ea58d363ad3f0174a5ced873556d
Алексей <tm23515417@gmail.com>
2024-07-22 21:55:25 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WinFormsApp_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lstSourse = new ListBox();
+            lstDest = new ListBox();
+            btnAddDest = new Button();
+            btnAddDestAll = new Button();
+            btnClearDest = new Button();
+            chbDelSourse = new CheckBox();
+            btnAddSource = new Button();
+            btnAddSourseAll = new Button();
+            pbSourse = new ProgressBar();
+            pbDest = new ProgressBar();
+            btnForm2 = new Button();
+            SuspendLayout();
+            // 
+            // lstSourse
+            // 
+            lstSourse.FormattingEnabled = true;
+            lstSourse.ItemHeight = 15;
+            lstSourse.Items.AddRange(new object[] { "Физика", "Химия", "Математика", "Черчение", "Музыка" });
+            lstSourse.Location = new Point(61, 28);
+            lstSourse.Name = "lstSourse";
+            lstSourse.SelectionMode = SelectionMode.MultiSimple;
+            lstSourse.Size = new Size(150, 259);
+            lstSourse.TabIndex = 0;
+            // 
+            // lstDest
+            // 
+            lstDest.FormattingEnabled = true;
+            lstDest.ItemHeight = 15;
+            lstDest.Location = new Point(322, 28);
+            lstDest.Name = "lstDest";
+            lstDest.Size = new Size(150, 259);
+            lstDest.TabIndex = 1;
+            // 
+            // btnAddDest
+            // 
+            btnAddDest.Location = new Point(237, 28);
+            btnAddDest.Name = "btnAddDest";
+            btnAddDest.Size = new Size(65, 23);
+            btnAddDest.TabIndex = 2;
+            btnAddDest.Text = "->";
+            btnAddDest.UseVisualStyleBackColor = true;
+            btnAddDest.Click += btnAddDest_Click;
+            // 
+            // btnAddDestAll
+            // 
+            btnAddDestAll.Location = new Point(237, 73);
+            btnAddDestAll.Name = "btnAddDestAll";
+            btnAddDestAll.Size = new Size(65, 23);
+            btnAddDestAll.TabIndex = 3;
+            btnAddDestAll.Text = "->>";
+            btnAddDestAll.UseVisualStyleBackColor = true;
+            btnAddDestAll.Click += btnAddDestAll_Click;
+            // 
+            // btnClearDest
+            // 
+            btnClearDest.Enabled = false;
+            btnClearDest.Location = new Point(237, 253);
+            btnClearDest.Name = "btnClearDest";
+            btnClearDest.Size = new Size(65, 23);
+            btnClearDest.TabIndex = 4;
+            btnClearDest.Text = "C";
+            btnClearDest.UseVisualStyleBackColor = true;
+            btnClearDest.Click += btnClearDest_Click;
+            // 
+            // chbDelSourse
+            // 
+            chbDelSourse.AutoSize = true;
+            chbDelSourse.Location = new Point(502, 30);
+            chbDelSourse.Name = "chbDelSourse";
+            chbDelSourse.Size = new Size(146, 19);
+            chbDelSourse.TabIndex = 5;
+            chbDelSourse.Text = "Удалить из источника";
+            chbDelSourse.UseVisualStyleBackColor = true;
+            // 
+            // btnAddSource
+            // 
+            btnAddSource.Enabled = false;
+            btnAddSource.Location = new Point(237, 114);
+            btnAddSource.Name = "btnAddSource";
+            btnAddSource.Size = new Size(65, 23);
+            btnAddSource.TabIndex = 6;
+            btnAddSource.Text = "<-";
+            btnAddSource.UseVisualStyleBackColor = true;
+            btnAddSource.Click += btnAddSource_Click;
+            // 
+            // btnAddSourseAll
+            // 
+            btnAddSourseAll.Enabled = false;
+            btnAddSourseAll.Location = new Point(237, 160);
+            btnAddSourseAll.Name = "btnAddSourseAll";
+            btnAddSourseAll.Size = new Size(65, 23);
+            btnAddSourseAll.TabIndex = 7;
+            btnAddSourseAll.Text = "<<-";
+            btnAddSourseAll.UseVisualStyleBackColor = true;
+            btnAddSourseAll.Click += btnAddSourseAll_Click;
+            // 
+            // pbSourse
+            // 
+            pbSourse.Location = new Point(64, 314);
+            pbSourse.Name = "pbSourse";
+            pbSourse.Size = new Size(147, 23);
+            pbSourse.Step = 20;
+            pbSourse.TabIndex = 8;
+            // 
+            // pbDest
+            // 
+            pbDest.Location = new Point(323, 313);
+            pbDest.Name = "pbDest";
+            pbDest.Size = new Size(149, 23);
+            pbDest.TabIndex = 9;
+            // 
+            // btnForm2
+            // 
+            btnForm2.Location = new Point(520, 82);
+            btnForm2.Name = "btnForm2";
+            btnForm2.Size = new Size(117, 23);
+            btnForm2.TabIndex = 10;
+            btnForm2.TabStop = false;
+            btnForm2.Text = "Открыть форму 2";
+            btnForm2.UseVisualStyleBackColor = true;
+            btnForm2.Click += btnForm2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnForm2);
+            Controls.Add(pbDest);
+            Controls.Add(pbSourse);
+            Controls.Add(btnAddSourseAll);
+            Controls.Add(btnAddSource);
+            Controls.Add(chbDelSourse);
+            Controls.Add(btnClearDest);
+            Controls.Add(btnAddDestAll);
+            Controls.Add(btnAddDest);
+            Controls.Add(lstDest);
+            Controls.Add(lstSourse);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox lstSourse;
+        private ListBox lstDest;
+        private Button btnAddDest;
+        private Button btnAddDestAll;
+        private Button btnClearDest;
+        private CheckBox chbDelSourse;
+        private Button btnAddSource;
+        private Button btnAddSourseAll;
+        private ProgressBar pbSourse;
+        private ProgressBar pbDest;
+        private Button btnForm2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿namespace WinFormsApp_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClearDest_Click(object sender, EventArgs e)
+        {
+            lstDest.Items.Clear(); // Очищаем коллекцию items
+            btnAddSource.Enabled = false;
+            btnAddSourseAll.Enabled = false;
+            pbDest.Value = 0;
+            pgValue();
+        }
+
+        private void btnAddDest_Click(object sender, EventArgs e)
+        {
+            //lstDest.Items.Add(lstSourse.SelectedItems[0]); // Добавляем выделенный Items            
+            if (lstSourse.Items.Count == 0)
+                btnAddDest.Enabled = false;
+            for (int i = 0; i < lstSourse.SelectedItems.Count; i++)
+            {
+                if (chbDelSourse.Checked)
+                    MoveItems(lstSourse, lstDest);
+                else
+                    lstDest.Items.Add(lstSourse.SelectedItems[i]);
+            }
+            if (lstDest.Items.Count != 0)
+            {
+                btnClearDest.Enabled = true;
+                btnAddSourseAll.Enabled = true;
+                btnAddSource.Enabled = true;
+            }
+            if (lstSourse.Items.Count == 0)
+            {
+                btnAddDest.Enabled = false;
+                btnAddDestAll.Enabled = false;
+            }
+            pgValue();
+        }
+
+        private void btnAddSource_Click(object sender, EventArgs e)
+        {           
+            if (lstSourse.Items.Count > 0)
+            {
+                btnAddDest.Enabled = true;
+                btnAddDestAll.Enabled = true;
+            }
+            for (int i = 0; i < lstDest.SelectedItems.Count; i++)
+            {
+                if (chbDelSourse.Checked)
+                    MoveItems(lstDest, lstSourse);
+                else
+                    lstDest.Items.Add(lstDest.SelectedItems[i]);
+            }
+            pgValue();
+        }
+
+        private void btnAddDestAll_Click(object sender, EventArgs e)
+        {
+            if (lstSourse.Items.Count == 0)
+                btnAddDestAll.Enabled = false;
+            lstDest.Items.AddRange(lstSourse.Items); // Добавляем весь список
+            if (chbDelSourse.Checked)
+            {
+                lstSourse.Items.Clear();
+                btnAddDest.Enabled = false;
+                btnAddDestAll.Enabled = false;
+            }
+            if (lstDest.Items.Count != 0)
+            {
+                btnClearDest.Enabled = true;
+                btnAddSourseAll.Enabled = true;
+                btnAddSource.Enabled = true;
+            }
+            pgValue();
+        }
+
+        private void btnAddSourseAll_Click(object sender, EventArgs e)
+        {
+            lstSourse.Items.AddRange(lstDest.Items);
+            btnAddDest.Enabled = true;
+            btnAddDestAll.Enabled = true;
+            if (chbDelSourse.Checked)
+            {
+                lstDest.Items.Clear();
+                btnAddSource.Enabled = false;
+                btnAddSourseAll.Enabled = false;
+            }
+            pgValue();
+        }
+        private void MoveItems(ListBox Sourse, ListBox Dest)
+        {
+            for (int i = 0; i < Sourse.SelectedItems.Count;)
+            {
+                Dest.Items.Add(Sourse.SelectedItems[i]);
+                Sourse.Items.Remove(Sourse.SelectedItems[i]);
+            }
+        }
+
+        private void pgValue() // Доработанный метод (перерасчёт списка > изначальных 5 items)
+        {
+            int p = lstDest.Items.Count * 20;
+            if (p > pbDest.Maximum)
+                pbDest.Maximum = p;
+            pbDest.Value = p;            
+            p = lstSourse.Items.Count * 20;
+            if (p > pbSourse.Maximum)
+                pbSourse.Maximum = p;
+            pbSourse.Value = p;            
+        }
+
+        Form form;
+
+        private void btnForm2_Click(object sender, EventArgs e)
+        {
+            if ((form is null) || (!form.Created))
+            {
+                form = new Form2(lstDest);
+                // form.ShowDialog(); // Только одно окно form2 (модальное окно)
+                form.Show(); // Form2 немодальная, можно открыть несколько окон
+            }
+        }
+    }
+}
